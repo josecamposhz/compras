0830135f002ac128c99eb15e705642f52a7c4d43
José Campos <josecamposh95@gmail.com>
2022-05-12 20:10:15 -0400

    update connection to postgress

--- db.js
+++ db.js
@@ -1,12 +1,23 @@
 const { Pool } = require('pg');
 
+// const pool = new Pool({
+//     user: "postgres",
+//     host: "localhost",
+//     password: "root",
+//     database: "super",
+//     port: 5432
+// })
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    password: "root",
-    database: "super",
-    port: 5432
-})
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 const getProductos = async () => {
     const { rows } = await pool.query({
